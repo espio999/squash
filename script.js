ae19ef4842f52d3fc6adddb18480f32ac8507b49
espio999 <espio999@users.noreply.github.com>
2023-08-14 21:22:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function MyResize()
+{
+	window.resizeTo(640, 480);
+}
+
+function ChangeFontColor(element)
+{
+	if (element.style.color == "orange"){
+		element.style.color = "white";
+	}
+	else{
+		element.style.color = "orange";
+	}
+}
